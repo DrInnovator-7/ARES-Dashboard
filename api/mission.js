eefc7762fab25031e4c8df9a74d600806f836228
Rajesh Kumar Pandey <bethegodofphysics@gmail.com>
2026-07-27 02:48:52 +0530

    API test

--- api/mission.js
+++ api/mission.js
@@ -1,39 +1,6 @@
-import { cert, getApps, initializeApp } from "firebase-admin/app";
-import { getDatabase } from "firebase-admin/database";
-
-if (getApps().length === 0) {
-  initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
-    databaseURL:
-      "httpshttps://ares-command-centre-default-rtdb.asia-southeast1.firebasedatabase.app/",
+export default function handler(req, res) {
+  return res.status(200).json({
+    success: true,
+    message: "API is working!"
   });
-}
-
-const db = getDatabase();
-
-export default async function handler(req, res) {
-  try {
-    const missionRef = db.ref("mission");
-
-    const snapshot = await missionRef.once("value");
-
-    const data = snapshot.val();
-
-    res.status(200).json({
-      success: true,
-      mission: data,
-    });
-
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
 }
