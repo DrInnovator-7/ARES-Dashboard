2f9948ecb1ba1a7377e8a033cb61aec305efa995
Rajesh Kumar Pandey <bethegodofphysics@gmail.com>
2026-07-27 02:53:40 +0530

    Check environment variables

--- api/mission.js
+++ api/mission.js
@@ -1,6 +1,10 @@
 export default function handler(req, res) {
   return res.status(200).json({
-    success: true,
-    message: "API is working!"
+    projectId: process.env.FIREBASE_PROJECT_ID || "MISSING",
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "MISSING",
+    privateKeyExists: !!process.env.FIREBASE_PRIVATE_KEY,
+    privateKeyLength: process.env.FIREBASE_PRIVATE_KEY
+      ? process.env.FIREBASE_PRIVATE_KEY.length
+      : 0,
   });
 }
